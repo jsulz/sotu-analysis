317f161bcdbca664adbc4e0f3b41d598b4d24db1
jsulz <jsulz@users.noreply.huggingface.co>
2024-08-28 04:15:57 +0200

    updates

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import gradio as gr
+from datasets import load_dataset
+
+# Load the dataset and convert it to a Pandas dataframe
+sotu_dataset = 'jsulz/state-of-the-union-addresses'
+dataset = load_dataset(sotu_dataset)
+df = dataset['train'].to_pandas()
+
+print(df.head(10))
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Hello " + name + ", you're cool!!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# Create a Gradio interface with blocks
+with  gr.Blocks() as demo:
+    with gr.Row():
+        gr.Markdown("# A Dashboard to Analyze the State of the Union Addresses")
+        demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
 demo.launch()
